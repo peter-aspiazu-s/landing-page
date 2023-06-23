de2b1d5588ad84d98bdd313efd7b8db162d3cd3a
Peter Aspiazu <paspiazusabando@gmail.com>
2023-06-23 16:44:18 -0500

    cambiando precio de logo

--- components/services/ServiceLogo.tsx
+++ components/services/ServiceLogo.tsx
@@ -4,7 +4,7 @@ export const ServiceLogo:FC = ():JSX.Element => {
   return (
     <div className='service-logo'>
         <div className='service-logo_container'>
-            <div className='service-logo_text'>💰Creamos logotipos a medida que reflejan la esencia y los valores de tu negocio. Cada diseño es único y exclusivo para ti. Obtén un logotipo profesional a un precio increíble de solo $70. Invierte en tu marca sin romper el banco.</div>
+            <div className='service-logo_text'>💰Creamos logotipos a medida que reflejan la esencia y los valores de tu negocio. Cada diseño es único y exclusivo para ti. Obtén un logotipo profesional a un precio increíble de solo $50. Invierte en tu marca sin romper el banco.</div>
             <a className='service-logo_link' href="https://wa.link/t0lz0i" target='_blank'>Cotizar mi logo</a>
         </div>
     </div>
